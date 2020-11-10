53e32a46886f944f8d2406515af6f819232301ba
surajkumar7252 <71683354+surajkumar7252@users.noreply.github.com>
2020-11-10 07:13:21 +0530

    Update AppTest.java

--- src/test/java/day26addressbookjdbcproblem/day26addressbookjdbc/AppTest.java
+++ src/test/java/day26addressbookjdbcproblem/day26addressbookjdbc/AppTest.java
@@ -1,20 +1,36 @@
 package day26addressbookjdbcproblem.day26addressbookjdbc;
 
-import static org.junit.Assert.assertTrue;
-
+import java.sql.SQLException;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+{    List<ContactDetails> listOfContacts; 
+      AddressBookJDBCService addressBookService=new AddressBookJDBCService();
+   
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    public void contactToAddressBookWhenAdded_shouldReturnTheNumberOfMatchedResults() throws SQLException {
+		ContactDetails munna = new ContactDetails("Munna", "Kumar", "Mithapur", "Gaya", "Bihar", 120012);
+		try {
+			listOfContacts = this.addressBookService.readContactList();
+			Assert.assertEquals(1, listOfContacts.size());
+			Assert.assertEquals(munna, listOfContacts.get(1));
+		} catch (AddressBookException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    @Test
+	public void updatingContactDetails() throws SQLException {
+		
+		try {
+			
+			this.addressBookService.updateAddressBookDetails("Shravan","Kumar","Paraiya", "Gaya", "Bihar", 585252);
+			this.addressBookService.readContactsDetails("Shravan","Kumar");
+			Assert.assertEquals(this.addressBookService.resultSetOpted.getString("FIRST_NAME "), "Shravan");
+		}catch(AddressBookException e) {
+			e.printStackTrace();
+		}
+	}
 }
